4a42c646f9f22b64639e70ebf1ca39a598614782
RyotoTomioka <tommy.ryoto93@gmail.com>
2017-08-30 02:09:19 +0900

    change default luminosity

--- src/Light.java
+++ src/Light.java
@@ -8,12 +8,8 @@ public class Light {
     private int signalA = 0;  // 白色信号値
     private int signalB = 0;  // 昼白色信号値
 
-    private double luminosity = 0;   // 光度
-    private double luminosityA = 0;  // 白色光度
-    private double luminosityB = 0;  // 電球色光度
-
     private double temperature = 4000;  // 色温度
-    private double lumPct = 3;        // 光度パーセント
+    private double lumPct = 500.;       // 光度パーセント
 
     public Light() {
         id = id_counter++;
