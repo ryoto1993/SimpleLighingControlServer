761a278c03e335094ffdd021b5e0258798e9de36
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2017-09-04 18:11:50 +0900

    change class name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 /**
  * Created by RyotoTomioka on 2017/06/12.
  */
-public class Manager {
+public class Main {
     public static void main(String[] args) {
         ILS ils = new ILS();
         ils.makeLight(36);
